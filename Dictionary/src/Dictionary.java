d06c4ce61ed386e2590c2ee55788d7318248808f
Nguyen Huu Minh Quang <36332303+quangnguyen99@users.noreply.github.com>
2018-10-13 20:49:41 +0700

    Add files via upload

---
+++ Dictionary/src/Dictionary.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.util.*;
+public class Dictionary {
+        public void saveWord(Map<String,String> map)
+        {
+                Word w = new Word();
+                try{
+                        FileReader fr = new FileReader("C:\\Users\\Admin\\IdeaProjects\\Dictionary\\dictionaries.txt");
+                        BufferedReader br = new BufferedReader(fr);
+                        String readWord;
+                        while ((readWord = br.readLine()) != null) {
+                                  int j=0;
+                                for(int i=0; i<readWord.length(); i++) {
+                                        j++;
+                                        if(Character.isSpaceChar(readWord.charAt(i))) {
+                                                break;
+                                        }
+                                }
+                                String tmpWord = readWord;
+                                w.word_target = tmpWord.substring(0,j-1);
+                                w.word_explain= readWord.substring(j);
+                               // w.word_target = String.valueOf(w.word_target.charAt(0)).toUpperCase() + w.word_target.substring(1,j-1);
+                                map.put(w.word_target, w.word_explain);
+                        }
+                        br.close();
+                        fr.close();
+                }catch (Exception e){
+                        System.out.println("ERROR");
+                }
+
+        }
+}
---
+++ Dictionary/src/DictionaryManagement.java
@@ -0,0 +1,128 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DictionaryManagement {
+
+        public static final Scanner scanner = new Scanner(System.in);
+
+        public String insertFromCommandline()
+        {
+                String word_target;
+                word_target = scanner.nextLine();
+                word_target = word_target.trim();
+                word_target = word_target.toLowerCase();
+                return word_target;
+        }
+
+        public String dictionaryLookup(Map<String,String>map)
+        {
+                DictionaryManagement dictionaryManagement = new DictionaryManagement();
+                System.out.println("Nhập từ cần dịch(Anh-Việt) ");
+                String enterWord = dictionaryManagement.insertFromCommandline();
+                String s;
+                 if(map.containsKey(enterWord))
+                {
+                        s = enterWord + " : " + map.get(enterWord);
+                }
+                  else
+                {
+                        s = "Không có từ cần tra";
+                }
+                return s;
+        }
+
+        public void dictionaryExportToFile(Map<String,String>map)
+        {
+
+                File file = new File("C:\\Users\\Admin\\IdeaProjects\\Dictionary\\1.txt");
+                FileWriter fr = null;
+                BufferedWriter br = null;
+                try{
+                        fr = new FileWriter(file);
+                        br = new BufferedWriter(fr);
+
+                        for(Map.Entry<String,String>entry: map.entrySet()) {
+                                String dataWithNewLine = entry.getKey()+" "+entry.getValue()+System.getProperty("line.separator");
+                                br.write(dataWithNewLine);
+                        }
+
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }finally{
+                        try {
+                                br.close();
+                                fr.close();
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
+                }
+
+        }
+        public void dictionaryExportToFile(String s)
+        {
+
+                File file = new File("C:\\Users\\Admin\\IdeaProjects\\Dictionary\\2.txt");
+                FileWriter fr = null;
+                BufferedWriter br = null;
+                try{
+                        if (!file.exists()) {
+                                file.createNewFile();
+                        }
+                        fr = new FileWriter(file);
+                        br = new BufferedWriter(fr);
+                        br.write(s);
+
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }finally{
+                        try {
+                                br.close();
+                                fr.close();
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
+                }
+
+        }
+
+
+        public void removeWord(Map<String,String> map)
+        {
+                System.out.print("Nhập từ Tiếng Anh muốn xóa: ");
+                String s = scanner.nextLine();
+                if(map.containsKey(s))
+                {
+                        map.remove(s);
+                }
+                else
+                {
+                        System.out.println("Từ "+ s + " Không có trong từ điển để xóa");
+                }
+        }
+
+        public void addWriteFile(String s)
+        {
+                try {
+                        File file = new File("C:\\Users\\Admin\\IdeaProjects\\Dictionary\\dictionaries.txt");
+                        if (!file.exists()) {
+                                file.createNewFile();
+                        }
+                        FileWriter fw = new FileWriter(file.getName(), true);
+                        BufferedWriter bw = new BufferedWriter(fw);
+                        String data = s + System.getProperty("line.separator");
+                        bw.write(data);
+                        bw.close();
+                        System.out.println("Đã Thêm thành công");
+                }
+
+                catch (Exception e) {
+                        e.printStackTrace();
+                }
+        }
+
+}
+
